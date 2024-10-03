aa2e63abd464201ba15a32dd519e703dda138d60
Chaman Chaudhary <153722807+chman-chaudhary@users.noreply.github.com>
2024-10-03 19:03:29 +0530

    Update ProjectCard.jsx

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -6,7 +6,7 @@ export default function ProjectCard({ project }) {
         <div className={`w-auto h-[280px] sm:h-[220px] md:h-[230px] lg:h-[300px] xl:h-[300px] flex flex-col justify-between ${project.card} card`}>
             <div className='flex justify-end gap-3 p-2'>
                 <a href={project.github} target='_blank'><i className="fa-brands fa-github text-md sm:text-md md:text-lg lg:text-xl text-black bg"></i></a>
-                <a href={project.website} target='_blank'><i className="fa-solid fa-link text-md sm:text-md md:text-lg lg:text-xl text-black bg"></i></a>
+                {project.website && <a href={project.website} target='_blank'><i className="fa-solid fa-link text-md sm:text-md md:text-lg lg:text-xl text-black bg"></i></a> }
             </div>
             <div className='flex justify-center items-center'>
                 <span className='text-2xl sm:text-lg md:text-lg lg:text-xl xl:text-3xl font-extrabold imp text-black bg'>{project.name}</span>

4435e5bb50fd3589b7ab2fecec2bb5711f0fe267
Chaman Chaudhary <153722807+chman-chaudhary@users.noreply.github.com>
2024-10-17 21:34:13 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,46 +4,46 @@ import ProjectCard from './ProjectCard'
 export default function Projects() {
 
     const projects = [
+         {
+            name: "GLA Codify",
+            skills: ["NEXT.js", "TypeScript", "Tailwind", "Shadcn"],
+            github: "https://github.com/chman-chaudhary/GLA-Codify-Ts",
+            card: "card-1",
+        },
         {
             name: "Job Portal Application",
             skills: ["MERN", "Tailwind", "UI/UX"],
             github: "https://github.com/chman-chaudhary/Job-Portal",
-            card: "card-1",
+            card: "card-2",
         },
         {
             name: "Globe Stay",
             skills: ["Node JS", "Express", "Mongodb", "BootStrap"],
             github: "https://github.com/chman-chaudhary/Globe-Stay",
             website: "https://globe-stay.onrender.com/listings",
-            card: "card-2",
+            card: "card-3",
         },
         {
             name: "Code Converter",
             skills: ["AI", "Next.js", "Shadcn Ui"],
             github: "https://github.com/chman-chaudhary/JavaScript-to-Python",
             website: "https://convertyourcode.vercel.app",
-            card: "card-3",
+            card: "card-4",
         },
         {
             name: "Portfolio",
             skills: ["React", "Tailwind", "JavaScript"],
             github: "https://github.com/chman-chaudhary/Portfolio",
             website: "https://portfolio-chaman-chaudhary.vercel.app/",
-            card: "card-4",
+            card: "card-5",
         },
         {
             name: "Weather App",
             skills: ["React", "JavaScript", "Tailwind"],
             github: "https://github.com/chman-chaudhary/WeatherApp",
             website: "https://weather-app-ashy-beta.vercel.app/",
-            card: "card-5",
-        },
-        {
-            name: "Currency Converter",
-            skills: ["Html", "Css", "JavaScript"],
-            github: "https://github.com/chman-chaudhary/Currency-Converter",
             card: "card-6",
-        },
+        }
     ]
 
     return (

01045e8b313690ce5df4d5136d45a1c23e8d403d
Chaman Chaudhary <153722807+chman-chaudhary@users.noreply.github.com>
2024-11-01 20:09:16 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
         <div className='w-[80%] mx-auto' id='about'>
             <span className='heading'>About -</span>
             <h3 className='text-2xl font-semibold pt-2 imp' >Hi, I'm Chaman.</h3>
-            <p className='leading-7 text-md text-justify' >I'm a passionate full-stack MERN developer with a strong foundation in building robust and scalable web applications. My expertise lies in crafting seamless user experiences using React, while efficiently managing backend operations with Node.js, Express, and MongoDB. With a solid grasp of Data Structures and Algorithms, I excel at optimizing application performance and solving complex problems. As a final year Bachelor of Computer Application student at GLA University, Mathura, I'm eager to contribute my skills to a dynamic team and take on challenging projects.</p >
+            <p className='leading-7 text-md text-justify' >I'm a passionate full-stack developer with a strong foundation in building robust and scalable web applications. My expertise lies in crafting seamless user experiences using React, while efficiently managing backend operations with Node.js, Express, and MongoDB. With a solid grasp of Data Structures and Algorithms, I excel at optimizing application performance and solving complex problems. As a final year Bachelor of Computer Application student at GLA University, Mathura, I'm eager to contribute my skills to a dynamic team and take on challenging projects.</p >
         </div >
     )
 }
